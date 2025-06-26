779109a57240d21ba92a7b516ce2e39c8dc16615
Bagusarya <aryabagus453@gmail.com>
2025-06-26 14:05:15 +0700

    first commit

---
+++ app/libs/product.js
@@ -0,0 +1,13 @@
+export const product = {
+    id: ~~(Math.random() * 100) + 1,
+    image: "/img.png",
+    name: "Playstation PS5 Portal 远程播放器",
+    price: 350000,
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa accusantium, aspernatur provident beatae corporis veniam atque facilis",
+    colors: [
+        { value: "putih", label: "Putih" },
+        { value: "biru", label: "Biru" },
+        { value: "coklat", label: "Coklat" },
+        { value: "kuning", label: "Kuning" }
+    ]
+}
